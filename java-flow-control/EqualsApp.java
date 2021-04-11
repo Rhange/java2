7cb65d2135b118b61cc3438ed3f9c3efe3675dc6
Rhange <rhange0511@gmail.com>
2021-04-11 14:56:31 +0900

    ==와 equals 비교에 대한 예제
    - String은 non-primitive value이다.
    - primitive value는 `stack`에 저장되는 반면 non-primitive value는 `heap` 영역에 저장된다.
    - `heap` 영역에 있는 데이터들은 그들의 주소(ref)를 stack에 있는 변수들에 제공한다.
    - 변수에 연결되어 있지 않은 heap 데이터들은 garbage로 분류된다.

---
+++ java-flow-control/EqualsApp.java
@@ -0,0 +1,17 @@
+
+public class EqualsApp {
+
+	public static void main(String[] args) {
+
+		String o1 = "java";
+		String o2 = new String("java");
+		System.out.println(o1 == o2);
+		
+		String o3 = "java2";
+		String o4 = "java2";
+		System.out.println(o3 == o4);
+
+		
+	}
+
+}
