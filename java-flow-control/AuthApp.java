80c3059b86b8351a902d6dba52aabbfefa3890a4
Rhange <rhange0511@gmail.com>
2021-04-11 14:20:19 +0900

    논리 연산자와 If문 활용 예제 코드
    - 논리연산자 `&&`(AND)를 활용하여 두 조건이 모두 만족할 경우에 코드를 실행하도록 할 수 있다.

--- java-flow-control/AuthApp.java
+++ java-flow-control/AuthApp.java
@@ -2,15 +2,29 @@
 public class AuthApp {
 
 	public static void main(String[] args) {
-		
-		System.out.println(args[0]);
-		
+		// User Id
 		String id = "Rhange";
 		String inputId = args[0];
 		
+		// User Password
+		String pass = "1111";
+		String inputPass = args[1];
+		
 		System.out.println("Hi!");
 		
-		if (inputId.equals(id)) {
+		// if (inputId == id) {
+//		if (inputId.equals(id)) {
+//			if (inputPass.equals(pass)) {
+//				System.out.println("Master!");
+//			} else {
+//				System.out.println("Wrong password!");
+//			}
+//		} else {
+//			System.out.println("Who are you?");
+//		}
+		
+		// Use logic operator
+		if (inputId.equals(id) && inputPass.equals(pass)) {
 			System.out.println("Master!");
 		} else {
 			System.out.println("Who are you?");
