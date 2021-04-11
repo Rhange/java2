de6840016c18679702f94923d292171464f7f00e
Rhange <rhange0511@gmail.com>
2021-04-11 13:54:17 +0900

    Comparison Operator 예제 코드

---
+++ java-flow-control/ComparisonOperatorApp.java
@@ -0,0 +1,13 @@
+
+public class ComparisonOperatorApp {
+
+	public static void main(String[] args) {
+		// Comparison Operator
+		System.out.println(1 > 1); // false
+		System.out.println(1 == 1); // true
+		System.out.println(1 < 2); // true
+		System.out.println(1 >= 1); // true
+
+	}
+
+}
