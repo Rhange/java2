c071e6bdff7e9f0cbab4cd33659a4ae08bd57f25
rhange <rhange0511@gmail.com>
2021-05-10 23:45:58 +0900

    Practice Coding 비밀번호 추가
    - 이중배열을 사용하여 유저 아이디와 패스워드 체크

--- java-flow-control/AuthApp3.java
+++ java-flow-control/AuthApp3.java
@@ -3,14 +3,31 @@ public class AuthApp3 {
 
 	public static void main(String[] args) {
 
-		String[] users = { "rhange", "tazo", "sang" };
+//		String[] users = { "rhange", "tazo", "sang" };
+		String[][] users = {
+				{"rhange", "1234"},
+				{"tazo", "2345"},
+				{"sang", "3456"}
+		};
+		
 		String inputId = args[0];
+		String inputPass = args[1];
 
 		boolean isLogined = false;
-		for (String name : users) {
-			String currentId = name;
-
-			if (currentId.equals(inputId)) {
+//		for (int i = 0; i < users.length; i++) {
+//			String[] current = users[i];
+//
+//			if (current[0].equals(inputId) && current[1].equals(inputPass)) {
+//				isLogined = true;
+//				break;
+//			}
+//		}
+		
+		for (String[] user : users) {
+			String userId = user[0];
+			String userPass = user[1];
+			
+			if (userId.equals(inputId) && userPass.equals(inputPass)) {
 				isLogined = true;
 				break;
 			}
