72642aff66f95a35fc6b8ace5e9326047abc9ce0
rhange <rhange0511@gmail.com>
2021-06-19 17:36:20 +0900

    부스트코스 java2 method

---
+++ ProjectMethod/FirstMethod.java
@@ -0,0 +1,10 @@
+
+public class FirstMethod {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Method");		// println is method
+		System.out.println(Math.floor(1.1));	// floor is method
+		
+	}
+}
