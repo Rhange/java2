c4c4e919f7f9d002c699c398cef1c37cc51fd5b7
rhange <rhange0511@gmail.com>
2021-06-19 17:36:37 +0900

    부스트코스 java2 method & refactoring

---
+++ ProjectMethod/WhyMethod.java
@@ -0,0 +1,21 @@
+
+public class WhyMethod {
+	
+	public static void main(String[] args) {
+		
+		//	100000000
+		printToTimesA();
+		//	100000000
+		printToTimesA();
+		//	100000000
+		printToTimesA();
+
+	}
+
+	public static void printToTimesA() {
+		System.out.println("-");
+		System.out.println("A");
+		System.out.println("A");
+	}
+
+}
