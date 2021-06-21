032db464c941fc0ee5ffcdb577295cc833a59bc3
rhange <rhange0511@gmail.com>
2021-06-22 05:36:27 +0900

    Refactoring with class

--- ProjectOOP/MyOOP.java
+++ ProjectOOP/MyOOP.java
@@ -1,26 +1,6 @@
-
-public class MyOOP {
-	public static void main(String[] args) {
-		// * method 안에 있는 variable은 해당 method 안에서만 쓸 수 있다. (유효 범위)
-		delimiter = "====";
-		
-		// ... 1억줄의 코드
-		printA();		
-		printA();
-		printB();		
-		printB();
-		
-		// ... 1억줄의 코드
-		delimiter = "****";
-		printA();		
-		printA();
-		printB();		
-		printB();
-		
-	}
-	
+class Print {
 	public static String delimiter = "";
-
+	
 	public static void printA() {
 		System.out.println(delimiter);
 		System.out.println("A");
@@ -33,5 +13,25 @@ public class MyOOP {
 		System.out.println("B");
 	}
 	
-	// ...
+	public static void printAll() {
+		printA();		
+		printA();
+		printB();		
+		printB();
+	}
+}
+
+
+public class MyOOP {
+	
+	public static void main(String[] args) {
+		
+		Print.delimiter = "====";
+		Print.printAll();
+		
+		Print.delimiter = "****";
+		Print.printAll();
+		
+	}
+	
 }
