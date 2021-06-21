d418ce0a573f5d255513bc74d9bcb0801dc7e394
rhange <rhange0511@gmail.com>
2021-06-22 05:41:16 +0900

    Updated method naming

--- ProjectOOP/MyOOP.java
+++ ProjectOOP/MyOOP.java
@@ -1,23 +1,23 @@
 class Print {
 	public static String delimiter = "";
 	
-	public static void printA() {
+	public static void A() {
 		System.out.println(delimiter);
 		System.out.println("A");
 		System.out.println("A");
 	}
 
-	public static void printB() {
+	public static void B() {
 		System.out.println(delimiter);
 		System.out.println("B");
 		System.out.println("B");
 	}
 	
 	public static void printAll() {
-		printA();		
-		printA();
-		printB();		
-		printB();
+		A();		
+		A();
+		B();		
+		B();
 	}
 }
 
