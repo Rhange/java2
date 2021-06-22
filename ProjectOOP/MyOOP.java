cca1e5e94345609455f9b5c3f332c8a7261ed7c8
rhange <rhange0511@gmail.com>
2021-06-22 15:34:43 +0900

    Instance save our time and life

--- ProjectOOP/MyOOP.java
+++ ProjectOOP/MyOOP.java
@@ -2,21 +2,28 @@
 public class MyOOP {
 	
 	public static void main(String[] args) {
+		// [type] [variable] = new [class]();
 		
-		Print.delimiter = "====";
-		Print.printAll();
+		// p1 is Instance
+		Print p1 = new Print();
+		p1.delimiter = "----";
+		p1.A();
+		p1.A();
+		p1.B();
+		p1.B();
 		
-		Print.delimiter = "****";
-		Print.printAll();
+		// p2 is Instance
+		Print p2 = new Print();
+		p2.delimiter = "****";
+		p2.A();
+		p2.A();
+		p2.B();
+		p2.B();
 		
-		Print.delimiter = "----";
-		Print.printAll();
-		
-		Print.delimiter = "****";
-		Print.printAll();
-		
-		Print.delimiter = "====";
-		Print.printAll();
+		p1.A();
+		p2.A();
+		p1.A();
+		p2.A();
 		
 		// ...
 		// 이 상황에서 벗어나게 해줄 Instance의 힘이 필요하다.
