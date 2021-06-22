7cedfd92d6c20d9a6f18cc0111b53096c7d35a30
rhange <rhange0511@gmail.com>
2021-06-22 15:21:28 +0900

    Instance의 필요성을 느끼게 해주는 코드

--- ProjectOOP/MyOOP.java
+++ ProjectOOP/MyOOP.java
@@ -9,6 +9,17 @@ public class MyOOP {
 		Print.delimiter = "****";
 		Print.printAll();
 		
+		Print.delimiter = "----";
+		Print.printAll();
+		
+		Print.delimiter = "****";
+		Print.printAll();
+		
+		Print.delimiter = "====";
+		Print.printAll();
+		
+		// ...
+		// 이 상황에서 벗어나게 해줄 Instance의 힘이 필요하다.
 	}
 	
 }
