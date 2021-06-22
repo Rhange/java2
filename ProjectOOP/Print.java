086b282fcf53a6ca19799bc1a3a9adc41c468d8f
rhange <rhange0511@gmail.com>
2021-06-22 15:34:20 +0900

    Instance can't use 'static' variable and method
    - class를 instance화 한 다음에 class 내부에 있는 variable과 method를 instance에서 사용하려면 'static'을 제거해주어야 한다.

--- ProjectOOP/Print.java
+++ ProjectOOP/Print.java
@@ -1,24 +1,20 @@
 
 class Print {
-	// delimiter, A, B are 'member' of class
-	public static String delimiter = "";
+	// remove 'static' for instance
+	public String delimiter = "";
 
-	public static void A() {
+	// remove 'static' for instance
+	public void A() {
 		System.out.println(delimiter);
 		System.out.println("A");
 		System.out.println("A");
 	}
 
-	public static void B() {
+	// remove 'static' for instance
+	public void B() {
 		System.out.println(delimiter);
 		System.out.println("B");
 		System.out.println("B");
 	}
 
-	public static void printAll() {
-		A();
-		A();
-		B();
-		B();
-	}
 }
