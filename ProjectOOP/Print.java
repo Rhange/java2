59363550463c96645405427c351500f273e9d427
rhange <rhange0511@gmail.com>
2021-06-26 15:49:21 +0900

    delete test code

--- ProjectOOP/Print.java
+++ ProjectOOP/Print.java
@@ -2,7 +2,7 @@
 class Print {
 	// remove 'static' for instance
 	public String delimiter = "";
-	
+
 	// Print 클래스와 이름이 같은 method
 	// construct method
 	// Print class가 호출되었을 때, 실행되는 method
@@ -12,7 +12,7 @@ class Print {
 		// this는 instance를 가리킨다.
 		this.delimiter = delimiter;
 	}
-	
+
 	// remove 'static' for instance
 	public void A() {
 		System.out.println(this.delimiter);
@@ -25,10 +25,6 @@ class Print {
 		System.out.println(this.delimiter);
 		System.out.println("B");
 		System.out.println("B");
-	}
-	
-	public init(String delimiter) {
-		this.delimiter = delimiter;
 	}
 
 }
