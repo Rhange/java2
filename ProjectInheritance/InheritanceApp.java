6ef2e9c0a0dd60482ee973f28ec83dbd5a6d1839
rhange <rhange0511@gmail.com>
2021-06-27 13:22:07 +0900

    Updated class Cal3
    - added 'minus' method

--- ProjectInheritance/InheritanceApp.java
+++ ProjectInheritance/InheritanceApp.java
@@ -5,17 +5,26 @@ class Cal {
 }
 
 class Cal3 extends Cal {
+	public int minus(int v1, int v2) {
+		return v1 - v2;
+	}
 }
 
 public class InheritanceApp {
 
 	public static void main(String[] args) {
 		Cal c = new Cal();
-		System.out.println(c.sum(2, 1));
+		System.out.println(c.sum(2, 1));	// 3
 
+		// Dose Cal2 class have sum method?
+		Cal2 c2 = new Cal2();
+		System.out.println(c2.sum(2, 2)); // 4
+		System.out.println(c2.minus(2, 2)); // 0
+		
 		// Dose Cal3 class have sum method?
 		Cal3 c3 = new Cal3();
-		System.out.println(c3.sum(2, 2));
+		System.out.println(c3.sum(2, 3));	// 5
+		System.out.println(c3.minus(2, 3));	// -1
 	}
 
 }
