ac879b8cff56b3810ba5eb07c5f64e72c4624df6
rhange <rhange0511@gmail.com>
2021-07-10 10:29:21 +0900

    class can have several interfaces
    - interface also can have 'variables' but this variable should be initialized.
    - class can't modify the variables in interface which is implemented in the class.
    - interface Name to be Capitalized and it's shape normally like '~able'

--- ProjectInterface/InterfaceApp.java
+++ ProjectInterface/InterfaceApp.java
@@ -1,16 +1,21 @@
 interface Calculable {
+	// interface에서 변수를 정의할 때는 value도 적는다.
+	double PI = 3.14;
 	int sum(int v1, int v2);
 }
 
-class RealCal implements Calculable {
+// interface의 이름은 대문자로 시작하고 형용사로 사용하는게 보편적
+interface Printable {
+	void print();
+}
+
+class RealCal implements Calculable, Printable {
 	public int sum(int v1, int v2) {
 		return v1 + v2;
 	}
-}
 
-class DummyCal implements Calculable {
-	public int sum(int v1, int v2) {
-		return 3;
+	public void print() {
+		System.out.println("This is RealCal!!");
 	}
 }
 
@@ -19,5 +24,7 @@ public class InterfaceApp {
 		// 더하기 기능이 있는 클래스를 만들어주세요.
 		RealCal c = new RealCal();
 		System.out.println(c.sum(2, 1));
+		c.print();
+		System.out.println("c.PI : " + c.PI);
 	}
 }
