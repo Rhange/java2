288b5f6a798dae8f683122e6420f37cb26817c1d
rhange <rhange0511@gmail.com>
2021-07-15 15:34:44 +0900

    catch (Exception e)
    - e is instance of Exception class
    - e has several methods (like printStackTrace)

--- ProjectException/ExceptionApp.java
+++ ProjectException/ExceptionApp.java
@@ -7,14 +7,16 @@ public class ExceptionApp {
 		// try, catch => handling exceptions
 		try {
 			System.out.println(2);
-			System.out.println(scores[3]);
+			// System.out.println(scores[3]);
 
 			// below codes are not going to execute
 			System.out.println(3);
 			System.out.println(2 / 0);
 			System.out.println(4);
 		} catch (ArithmeticException e) {
-			System.out.println("계산이 잘못된 것 같습니다.");
+			System.out.println("계산이 잘못된 것 같습니다. 이유 : " + e.getMessage());
+			e.printStackTrace(); // console에 error를 보여주도록 함. 프로그램이 멈추지는 않음
+								 // 일반 사용자에게 보여지면 보완이슈가 발생할 수 있는데, logging 시스템 API를 활용하면 좋다.
 		} catch (Exception e) {
 			System.out.println("뭔가가 이상합니다. 오류가 발생했거든요");
 		}
